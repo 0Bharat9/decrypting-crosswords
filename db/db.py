e4c7f867f89fc17a5d692c64708745703cf02f06
chandanbir <chandanbirsingh9@gmail.com>
2024-06-18 23:45:08 +0530

    update DB settings

--- db/db.py
+++ db/db.py
@@ -4,9 +4,7 @@ from pymongo.server_api import ServerApi
 
 
 ### MongoDB Stuff
-db_user = getenv("DBUSER")
-db_pass = getenv("DBPASS")  
-uri = "mongodb+srv://"+str(db_user)+":"+str(db_pass)+"@cluster0.ebzle64.mongodb.net/"
+uri  = getenv("URI")
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["crossword-solver"]
 
